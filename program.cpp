0abed79319d9163e2b203cf9a7fa5d5d893af7a5
IlyxChxliy <ilyachaily2006@gmail.com>
2025-12-01 18:56:39 +0500

    Update program.cpp
    Написал каркас программы

--- program.cpp
+++ program.cpp
@@ -1,1 +1,25 @@
-Hello world
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void readFromFile(vector<string> &lines) {
+    // Функция 1: чтение из файла
+}
+
+void printToScreen(const vector<string> &lines) {
+    // Функция 2: вывод на экран
+}
+
+void writeToFile(const vector<string> &lines) {
+    // Функция 3: запись в файл
+}
+
+int main() {
+    vector<string> lines;
+    readFromFile(lines);
+    printToScreen(lines);
+    writeToFile(lines);
+    return 0;
+}
